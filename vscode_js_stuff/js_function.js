839d9f697d4087a51ed85132f6f8abcc37f1123e
gar821 <joshuagurara@gmail.com>
2025-04-09 09:16:50 -0400

    functions & cirrcle

---
+++ vscode_js_stuff/js_function.js
@@ -0,0 +1,21 @@
+
+function hi(name){
+    console.log("hi " + name);
+}
+
+hi("Joshua");
+hi("E");
+
+//new func called f
+//f(x) = x + 1
+function f(x){
+    console.log(x+1);
+}
+
+f(2);
+
+function g(x,y){
+    console.log(x+y);
+}
+g(5,5);
+
