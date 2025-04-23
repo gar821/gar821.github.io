2bed2f5e8502357eb1bf07a7fc910f0cdf6457e7
gar821 <joshuagurara@gmail.com>
2025-04-23 08:21:35 -0400

    snowman eyes and smile classwork

--- vscode_js_stuff/snowman.js
+++ vscode_js_stuff/snowman.js
@@ -22,3 +22,28 @@ function snowman(x,y){
 snowman(200,200);
 snowman(100,200);
 snowman(300,200);
+
+function eyes(x,y) {
+    ctx.fillStyle= "black";
+    ctx.beginPath();
+    ctx.arc(x-7,y-5,3,0,2*Math.PI);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(x+7,y-5,3,0,2*Math.PI);
+    ctx.fill();
+}
+
+eyes(200, 200);
+eyes(100, 200);
+eyes(300, 200);
+
+function smile(x, y) {
+    ctx.strokeStyle = "black"; 
+    ctx.beginPath();
+    ctx.arc(x, y + 5, 10, 0, Math.PI, false);
+    ctx.stroke();
+}
+
+smile(200, 200);
+smile(100, 200);
+smile(300, 200);
