3e09783798a0d6a137e226ab7c4052dde7ba8034
gar821 <joshuagurara@gmail.com>
2025-04-25 08:53:14 -0400

    going to field trip so we stop here

--- vscode_js_stuff/animation.js
+++ vscode_js_stuff/animation.js
@@ -6,9 +6,31 @@ let y = 0;
 let dx = 5;
 let dy = 1;
 
+//we'll represent the player as an object
+const player = {
+    //variables use key:value pair syntax
+    x:200,
+    y:200,
+    color:'green',
+    speed:3
+}
+
+function drawPlayer(){
+    //to access a variable from the player object
+    //we uset his syntax:
+    //player.x
+    ctx.fillStyle = player.color
+    ctx.beginPath();
+    ctx.arc(
+        player.x,player.y,
+        20,0,2*Math.PI
+    )
+    ctx.fill();
+}
+
 //define functions
 function drawRect(x,y) {
-    console.log("drawing rect");
+    //console.log("drawing rect");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(x,y,50,50);
@@ -17,6 +39,7 @@ function drawRect(x,y) {
 
 function animate() {
     drawRect(x,y);
+    drawPlayer();
 
     // TODO: Add some code here 
     //  that will change the rectangle's position
@@ -37,6 +60,20 @@ if(y<0){
 }
     requestAnimationFrame(animate);
 }
+
+/*
+* Handle Keyboard
+    To handle an event, we need two things:
+        - event handler - does things because of the event
+        - event listener - notices when an event happens
+            , and calls the handler
+*/
+
+function handleKeyPress(e){
+    console.log(e.key);
+}
+
+document.addEventListener('keydown', handleKeyPress);
 
 
 //call our function
