575af8de45fe3c8786b01e9d634884b3a7af1b9b
gar821 <joshuagurara@gmail.com>
2025-05-07 08:33:52 -0400

    target data

--- vscode_js_stuff/game.js
+++ vscode_js_stuff/game.js
@@ -1,3 +1,17 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-console.log("this works");
+
+function drawBackground(){
+    ctx.fillStyle= "Gray";
+    ctx.fillRect(0,0,400,600);
+}
+drawBackground();
+
+const targetRadius=30;
+const targetY=50;
+const targets = [
+    { x: 50, color: 'rgba(255, 99, 71, 1)' }, // bright red
+    { x: 150, color: 'rgba(30, 144, 255, 1)' }, // bright blue
+    { x: 250, color: 'rgba(50, 205, 50, 1)' }, // bright green
+    { x: 350, color: 'rgba(255, 215, 0, 1)' }  // bright yellow
+];
