e1c7c1439e5e8bf03bff3e3d729fc408e8c8e9ca
gar821 <joshuagurara@gmail.com>
2025-05-07 08:59:11 -0400

    notes & move, also animate ebcause i definently need that

--- vscode_js_stuff/game.js
+++ vscode_js_stuff/game.js
@@ -5,7 +5,6 @@ function drawBackground(){
     ctx.fillStyle= "Gray";
     ctx.fillRect(0,0,400,600);
 }
-drawBackground();
 
 const targetRadius=30;
 const targetY=50;
@@ -23,5 +22,37 @@ function drawTargets(){
       ctx.fill();
     });
 }
-drawTargets();
 
+const noteRadius = targetRadius; //matching
+let notes = [
+    { x: 50, y:550, color: 'rgba(255, 99, 71, 1)' }, //  red note
+    { x: 150, y:500, color: 'rgba(30, 144, 255, 1)' }, //  blue note
+    { x: 250, y:450, color: 'rgba(50, 205, 50, 1)' }, //  green note
+    { x: 350, y:400, color: 'rgba(255, 215, 0, 1)' }  //  yellow note
+];
+function drawNotes() {
+    notes.forEach(note=>{
+        ctx.beginPath();
+        ctx.arc(note.x,note.y,noteRadius,0,Math.PI*2); //circle
+        ctx.fillStyle = note.color;
+        ctx.fill();
+    });
+}
+
+function updateNotes(){
+    notes.forEach(note=>{
+        note.y -=2; //move each note up by 2 px
+    });
+}
+
+function animate(){ // every 1/60 sec
+    ctx.clearRect(0,0,400,600);
+
+    drawBackground();
+    drawTargets();
+    drawNotes();
+
+    updateNotes();
+    requestAnimationFrame(animate);
+}
+animate();
